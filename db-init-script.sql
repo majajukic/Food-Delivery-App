013731fc585689271d869c8479a01129750ab16c
Maja Jukic <mjukic2000@gmail.com>
2024-12-10 14:11:45 +0100

    Creating docker images from services and adding docker-compose.yml file as well as db-init-script.sql file to root

---
+++ db-init-script.sql
@@ -0,0 +1,14 @@
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'RestaurantDb')
+BEGIN
+    CREATE DATABASE RestaurantDb;
+END
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'DeliveryDb')
+BEGIN
+    CREATE DATABASE DeliveryDb;
+END
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'OrderDb')
+BEGIN
+    CREATE DATABASE OrderDb;
+END
